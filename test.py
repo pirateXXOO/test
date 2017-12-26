13d8831139563aa0f6eea15c686b912f68245c14
pirateXXOO <piratexxoo@gmail.com>
2017-12-27 00:26:19 +0800

    variable

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 print("Hello World!")
 
 print("3乘以4=",3*4)
@@ -32,8 +33,22 @@ del age
 print("ls")
 
 print(3**3)
-
+'''
 '''This line will not be printed'''
 
+death_age = 80
 
+name = input("Your name:")
+age = input("Your age:")
+#age=int(age)
+'''
+print(name)
+print(age)
+'''
+print(type(name))
+print(type(age))
+print("Your anme is : ",name)
+print("Your age is :",age)
+#print("You can live ",death_age - int(age)," years old.")
+print("You can live " + str(death_age - int(age)) + " years old.")
 
