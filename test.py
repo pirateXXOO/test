cb5c989ae0608e559d4a91d2f3b579a96ac7c382
pirateXXOO <piratexxoo@gmail.com>
2017-12-27 00:04:32 +0800

    second commit

--- test.py
+++ test.py
@@ -1,6 +1,39 @@
 print("Hello World!")
+
 print("3乘以4=",3*4)
+
 print(3*4*5*6/2)
+
 x = 3
 y = 4
 print("x乘以y=",x*y)
+
+name = "Alix"
+print(name)
+
+print("pie")
+
+print("PIE")
+
+name="Jack"
+name2=name
+print(name)
+print(name2)
+
+name="Tom"
+print(name)
+print(name2)
+
+
+age=21
+print(age)
+del age
+# pirnt(age)
+print("ls")
+
+print(3**3)
+
+'''This line will not be printed'''
+
+
+
