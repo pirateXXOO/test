cc000c200d07b9173da3053f6f6fdce572cb5860
pirateXXOO <piratexxoo@gmail.com>
2018-01-23 14:49:31 +0800

    python class

---
+++ class.py
@@ -0,0 +1,45 @@
+# i = 0
+# while i < 100:
+#     print(i)
+#     print("nihao woshi wangshuai")
+#     i += 1
+
+# name = input("Please input your name: ")
+# print("Your name is : " + name)
+# print(name)
+
+# a = 1
+# b = 2
+# c = a and b
+# print(c)
+
+# from func import printWangshuai
+# nihao = input("Pleaes input nihao : ")
+# printWangshuai(nihao)
+
+# a = [0,1,2,3,4,5]
+# a.append(3)
+# print(a[-1])
+
+# tuple = (1,2,3)
+# print(tuple)
+# print(max(tuple))
+
+# A = 'A'
+# scores = {'A':95,'B':75,'C':43}
+# print(scores)
+#
+# if A in scores:
+#     print(scores[A])
+#
+# print(scores.keys())
+
+
+a = 1
+b = 2
+c = a and b
+print(c)
+
+
+
+
