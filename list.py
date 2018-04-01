5a7fd4731c683bb2f7a2cb282e408cf0b741d687
pirateXXOO <piratexxoo@gmail.com>
2018-04-01 23:30:50 +0800

    list practice

---
+++ list.py
@@ -0,0 +1,57 @@
+# Author:Jack
+# Date:2018/4/1
+
+a = ['wuchao','sanpang','jinxing','xiaohu','sanpang','ligang']
+b=['1','2','3']
+# print(a[2:4])
+# print(a[2:])
+
+# a.append("shell")
+# print(a)
+#
+# a.insert(1,"man")
+# print(a)
+#
+# print(a.index("shell"))
+#
+#
+# a[2]="shell"
+# print(a)
+#
+# a.remove("man")
+# print(a)
+#
+# a.pop()
+# print(a)
+#
+# a.pop(1)
+# print(a)
+#
+# a.pop(1)
+# print(a)
+#
+# del a[0]
+# print(a)
+#
+# print(a.count("sanpang"))
+#
+# a.extend(b)
+# print(a)
+
+# print(a.index('sanpang'))
+#
+# a_first_index = a.index('sanpang')
+# print(a_first_index)
+# c = a[a.index("sanpang")+1:]
+# print(c)
+# c_first_index = c.index('sanpang')
+# print(c_first_index)
+# a_secend_index = a_first_index + c_first_index + 1
+# print(a_secend_index)
+
+# a.reverse()
+# print(a)
+# a.sort()
+# print(a)
+# a.sort(reverse=True)
+# print(a)
