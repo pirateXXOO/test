33258ca7de942a53417311738c7e67223027912f
pirateXXOO <piratexxoo@gmail.com>
2018-04-02 22:40:36 +0800

    Tuples test

---
+++ day06/tuples.py
@@ -0,0 +1,14 @@
+# Author:Jack
+# Date:2018/4/2
+
+# a = (1,2,3,4)
+# print(a[1:2])
+#
+# product = [('Mac',5000),('bicycle','400')]
+# print(product[1][1])
+#
+# for i,j in enumerate(product,1):
+#     print(i,"\t",j)
+
+
+
