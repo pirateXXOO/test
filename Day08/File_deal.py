fb22fd03f7ec1970b09af911e5f1d2440d11c98a
pirateXXOO <piratexxoo@gmail.com>
2018-07-11 20:52:13 +0800

    day 8 file deal 4

--- Day08/File_deal.py
+++ Day08/File_deal.py
@@ -62,13 +62,69 @@
 # f.close()
 
 
-f=open('test','r')
-print(f.tell())
-print(f.read(3))
-print(f.tell())
-f.seek(0)
-print(f.tell())
-print(f.read(4))
-f.close()
+# f=open('test','r')
+# print(f.tell())
+# print(f.read(3))
+# print(f.tell())
+# f.seek(0)
+# print(f.tell())
+# print(f.read(4))
+# f.close()
 
 
+# import sys,time
+# for i in range(30):
+#     sys.stdout.write("*")
+#     sys.stdout.flush()
+#     time.sleep(0.2)
+
+# import sys,time
+# for i in range(30):
+#     print("*",end="",flush=True)
+#     time.sleep(0.1)
+
+# f=open("test",'w')
+# d=f.truncate(5)
+# print(d)
+# print(f)
+# f.close()
+
+# f=open('test','a+',encoding='utf8')
+# print(f.readline())
+# print(f.tell())
+# f.write('yuefei')
+# f.seek(0)
+# print(f.tell())
+# print(f.readline())
+# print(f.tell())
+# f.close()
+
+# f=open('test','r+',encoding='utf8')
+# number = 0
+# for line in f:
+#     number+=1
+#     if number == 6:
+#         f.write('alex')
+# f.close()
+
+# f=open('test','r',encoding='utf-8')
+# d=open('test3','w',encoding='utf-8')
+# number=0
+# for i in f:
+#     number+=1
+#     if number==5:
+#         i=''.join([i.strip(),'shellllkjsdijfijei'])
+#     d.write(i)
+# d.close()
+# f.close()
+
+# f=open('test','r')
+# f.close()
+# with open('test','r') as f:
+#     f.readline()
+#     f.read()
+# print('hello')
+
+with open('test','r') as f_read , open('test3','w') as f_write:
+    for line in f_read:
+        f_write.write(line)
